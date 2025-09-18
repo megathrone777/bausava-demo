9bb96624730dfc19aa619f0ea1ffc7f6eeebc743
Andrii Taranenko <megathrone3333@gmail.com>
2025-09-18 20:32:54 +0200

    Add build

--- config.php
+++ config.php
@@ -1,13 +1,18 @@
 <?php
   require_once $_SERVER["DOCUMENT_ROOT"] . "/lib/rb-mysql.php";
 
-	$env = parse_ini_file(".env");
-  $dbHost = $env["DB_HOST"];
-  $dbName = $env["DB_NAME"];
-  $dbUser = $env["DB_USER"];
-  $dbPassword = $env["DB_PASSWORD"];
+	// $env = parse_ini_file(".env");
+  // $dbHost = $env["DB_HOST"];
+  // $dbName = $env["DB_NAME"];
+  // $dbUser = $env["DB_USER"];
+  // $dbPassword = $env["DB_PASSWORD"];
 
-  R::setup("mysql:host=db.fr-pari1.bengt.wasmernet.com;dbname=bausava", "c4618ff67a6880005d5ad3f72fd0", "068cc461-8ff6-7e0e-8000-b3263cbaf1bd");
+  $dbHost = "db.fr-pari1.bengt.wasmernet.com";
+  $dbName = "bausava";
+  $dbUser = "c4618ff67a6880005d5ad3f72fd0";
+  $dbPassword = "068cc461-8ff6-7e0e-8000-b3263cbaf1bd";
+
+  R::setup("mysql:host=" . $dbHost . ";dbname=" . $dbName, $dbUser, $dbPassword);
   $strings = R::findAll("strings");
   set_include_path("src/");
 
